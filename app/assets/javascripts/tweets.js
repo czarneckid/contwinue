69c24cd8933d510b42b083380b822ced33377edf
Gillon <bioslippery@gmail.com>
2013-04-15 13:00:54 -0400

    Minor tidy-up

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,15 +1,13 @@
 $(function(){
     var charLimit = 140;
-    var tweetCount = 0;
     var currentCount = $("textarea[name=tweet]").val().length;
+    var tweetCount = Math.ceil(currentCount / charLimit);
 
-	$(".count span").text(currentCount + "/140 [Tweets: "+Math.ceil(currentCount / charLimit)+"]");
+	$(".count span").text(currentCount + "/140 [Tweets: "+tweetCount+"]");
     
     $("textarea[name=tweet]").keyup(function(){
-
        currentCount = $(this).val().length;
-       if(currentCount > 0) { tweetCount = Math.ceil(currentCount / charLimit);}
-       
+       tweetCount = Math.ceil(currentCount / charLimit);
        $(".count span").text(currentCount + "/140 [Tweets: "+tweetCount+"]");
          
     });
